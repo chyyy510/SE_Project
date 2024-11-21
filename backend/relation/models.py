bce7e8bdd75b276042594392b8c51528ce334583
chyyy510 <kmsmchyyy510@gmail.com>
2024-11-21 13:47:11 +0800

    创建engagement模型，管理用户参与实验

---
+++ backend/relation/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from appuser.models import User
+from experiment.models import Experiment
+
+
+# Create your models here.
+class Engagement(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)  # TODO:
+    experiment = models.ForeignKey(
+        Experiment, on_delete=models.SET_NULL, null=True
+    )  # TODO:
+    status_choice = {
+        ("user-qualification", "用户资质待审核"),
+        ("experiment-result", "实验结果待审核"),
+        ("finish", "已完成"),
+    }
+    status = models.CharField(max_length=24, choices=status_choice)
