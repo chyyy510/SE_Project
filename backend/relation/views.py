a187a7c74d8e409c628246b040cdcc2b6f866ec0
chyyy510 <kmsmchyyy510@gmail.com>
2024-11-26 11:23:43 +0800

    查看登录用户的已创建实验列表

--- backend/relation/views.py
+++ backend/relation/views.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from relation.models import Engagement
 from experiment.models import Experiment
+from experiment.views import ExperimentPagination
 from relation.serializers import EngagementSerializer
+from experiment.serializers import ExperimentSerializer
 
 from django.contrib.auth.models import AnonymousUser
 
@@ -70,10 +72,32 @@ class EngagementList(generics.GenericAPIView):
             )
         user = request.user
         engagements = Engagement.objects.filter(user=user).order_by("id")
+        # experiments_id = engagements.values("experiment")
+        experiment_ids = engagements.values_list("experiment_id", flat=True).distinct()
 
-        paginator = EngagementPagination()
-        paginated_engagements = paginator.paginate_queryset(engagements, request)
+        # 根据 experiment_id 查询对应的 Experiment 信息
+        experiments = Experiment.objects.filter(id__in=experiment_ids).order_by("id")
 
-        serializer = EngagementSerializer(paginated_engagements, many=True)
+        paginator = ExperimentPagination()
+        paginated_experiments = paginator.paginate_queryset(experiments, request)
 
+        serializer = ExperimentSerializer(paginated_experiments, many=True)
+
+        return paginator.get_paginated_response(serializer.data)
+
+
+class CreationList(generics.GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        if isinstance(request.user, AnonymousUser):
+            return Response(
+                {"detail": "Authentication required"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+        user = request.user
+        experiments = Experiment.objects.filter(creator=user).order_by("id")
+
+        paginator = ExperimentPagination()
+        paginated_experiments = paginator.paginate_queryset(experiments, request)
+
+        serializer = ExperimentSerializer(paginated_experiments, many=True)
         return paginator.get_paginated_response(serializer.data)
